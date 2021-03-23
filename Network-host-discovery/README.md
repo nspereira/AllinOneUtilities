04c6cab6790949a018267ba1da78d33f258c5859
Nuno Pereira <nuno.pereira@xpand-it.com>
2021-03-23 23:24:48 +0000

    minor changes on host discover component

--- Network-host-discovery/README.md
+++ Network-host-discovery/README.md
@@ -1,7 +1,15 @@
-# Host discovery Tool
+# Host discovery Tool Beta version
 
-Host discovery tool is capable of detecting any new entry on your network and also capable of detecting ARP Spoofing attacks, it works as a daemon/service
+Host discovery tool is capable of detecting any new entry on your network and also capable of detecting ARP Spoofing attacks, it works as a daemon
+
+### Features
+
+-Host discovery:
+  -Passive scan with details(hostname, ip, mac addr, mac vendor, ports, OS fingerprint)
+    -ARP, DHCP, NBNS
+  -Active scan(ICMP)
+-ARP poisoning detection
 
 ## Usage
 
-`sudo python3 host_discovery.py <interface>`
+`sudo python3 host_discovery.py <network interface>`
--- Network-host-discovery/host_detector.py
+++ Network-host-discovery/host_detector.py
@@ -1,10 +1,15 @@
 import sys
 import os
+import json
+from mac_vendor_lookup import AsyncMacLookup
 #from notifypy import Notify
 import scapy.all as scapy 
 
 n_interface = sys.argv[1] or ''  
 broadcast = "ff:ff:ff:ff:ff:ff"
+ipaddr_list = []
+
+print("[+] Initializing detection...")
 
 if os.geteuid() != 0:
     exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'\n...Exiting")
@@ -19,6 +24,13 @@ op_sys = {
         2: 'Windows'
 }
 
+async def mac_lookup(addr):
+    mac_addr = AsyncMacLookup()
+    print("[+] Updating Mac Address vendors...")
+    mac_addr.update_vendors()
+    print("[+] Update finished...")
+    print(await mac_addr.lookup(addr))
+
 def check_os():
     if sys.platform == op_sys.keys(0):
         print('Its linux')
@@ -27,6 +39,9 @@ def check_os():
     elif sys.platform == op_sys.keys(2):
         print('its windows')
 
+def save_data(ipaddr_list):
+    with open('data.json', 'w', encoding='utf-8') as f:
+        json.dump(ipaddr_list, f, ensure_ascii=False, indent=4)
 
 def get_mac(ipadd):
     arp_request = scapy.ARP(pdst=ipadd) 
@@ -38,16 +53,16 @@ def get_mac(ipadd):
   
 
 def detect_hosts(packet):
-    if scapy.TCP in packet and packet[scapy.TCP].flags & 2:
-        src = packet.sprintf('{IP:%IP.src%}{IPv6:%IPv6.src%}')
+    '''if scapy.TCP in packet and packet[scapy.TCP].flags & 2:
+        src = packet.sprintf('{IP:%IP.src% --> %IP.dst%}{IPv6:%IPv6.src%}')
+        ipaddr_list.append(src)
+        save_data(ipaddr_list)
         print(src)
-        alert_notifications('{IP:%IP.src%}')
-
-    '''if packet.haslayer(scapy.TCP) and packet[scapy.TCP].op == 2: 
+        #alert_notifications('{IP:%IP.src%}')'''
+    if packet.haslayer(scapy.TCP) and packet[scapy.TCP].op == 2: 
         originalmac = get_mac(packet[scapy.ARP].psrc) 
         responsemac = packet[scapy.ARP].hwsrc 
-        alert_notifications()'''
-
+        print(originalmac, responsemac)
 
 def alert_notifications(ip_src):
     notification = Notify('teste')
